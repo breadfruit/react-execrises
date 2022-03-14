24443f73a316aac599b546524898ca1ca8e17b00
美式因子 <67834178+breadfruit@users.noreply.github.com>
2022-03-14 16:29:18 +0800

    update

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyInput from "./components/input/";
+import './App.css';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyInput />
+    </div>
+  );
+}
+
+export default App;
