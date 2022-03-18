584645ef08fbfe93ee619acf42f2aa3b609a2b95
wengjq <wengjq@mingyuanyun.com>
2022-03-18 16:31:08 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import MyInput from "./components/input/";
 import Clock from "./components/clock/";
 import Content from "./components/officialCase/useContext";
-
+import Input from "./components/input/input"
 import './App.css';
 import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
-      <MyInput />
       <Clock />
       <Content />
+      <h3>==输入框==</h3>
+      <Input />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/tag/taginput.tsx
@@ -0,0 +1,1 @@
+function myTag() {};export default myTag;
