f20f228ba240a16bf7ef50397e7926a20154bbd3
wengjq <wengjq@mingyuanyun.com>
2022-05-05 21:48:34 +0800

    vv: first

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,1 +1,28 @@
-import { RootState } from '../../app/store'import { useSelector, useDispatch } from 'react-redux'import { decrement, increment } from './counterSlice'import { useAppSelector, useAppDispatch } from '../../app/hook'export function Counter() {    //可以拿到仓库里面的数据    const count = useAppSelector((state) => state.counter.value);    //ui更新，触发store数据更新    const dispatch = useAppDispatch();    return (        <div>            <button                aria-label="Increment value"                onClick={() => dispatch(increment())}            >                Increment            </button>            <button                aria-label="Increment value"                onClick={() => dispatch(decrement())}            >                Decrement            </button>        </div>    )}
+import { RootState } from '../../app/store'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './counterSlice'
+import { useAppSelector, useAppDispatch } from '../../app/hook'
+export function Counter() {
+    //可以拿到仓库里面的数据
+    const count = useAppSelector((state) => state.counter.value);
+    //ui更新，触发store数据更新
+    const dispatch = useAppDispatch();
+    return (
+        <div>
+            <button
+                aria-label="Increment value"
+                onClick={() => dispatch(increment())}
+            >
+                Increment
+            </button>
+            <button
+                aria-label="Increment value"
+                onClick={() => dispatch(decrement())}
+            >
+                Decrement
+            </button>
+        </div>
+    )
+}
+
+//vv分支 第一次变动
