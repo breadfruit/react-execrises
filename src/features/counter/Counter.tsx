c7bb947931655aa80bef4a338b10723dcc75565a
wengjq <wengjq@mingyuanyun.com>
2022-05-05 22:45:05 +0800

    ww:first

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,1 +1,26 @@
-import { RootState } from '../../app/store'import { useSelector, useDispatch } from 'react-redux'import { decrement, increment } from './counterSlice'import { useAppSelector, useAppDispatch } from '../../app/hook'export function Counter() {    //可以拿到仓库里面的数据    const count = useAppSelector((state) => state.counter.value);    //ui更新，触发store数据更新    const dispatch = useAppDispatch();    return (        <div>            <button                aria-label="Increment value"                onClick={() => dispatch(increment())}            >                Increment            </button>            <button                aria-label="Increment value"                onClick={() => dispatch(decrement())}            >                Decrement            </button>        </div>    )}
+import { decrement, increment } from './counterSlice'
+import { useAppDispatch } from '../../app/hook'
+export function Counter() {
+    //ui更新，触发store数据更新
+    const dispatch = useAppDispatch();
+    return (
+        <div>
+            <button
+                aria-label="Increment value"
+                onClick={() => dispatch(increment())}
+            >
+                Increment
+            </button>
+            <button
+                aria-label="Increment value"
+                onClick={() => dispatch(decrement())}
+            >
+                Decrement
+            </button>
+        </div>
+    )
+}
+
+//vv分支 第一次变动
+//vv分支 第三次变动
+//ww分支 第一次变动
