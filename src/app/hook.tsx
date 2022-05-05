5d71970d7ece34f3dfb3c0f7706c6ffcf8ef4f04
wengjq <wengjq@mingyuanyun.com>
2022-05-05 21:23:31 +0800

    测试2第二次提交

--- src/app/hook.tsx
+++ src/app/hook.tsx
@@ -3,3 +3,4 @@ import type {RootState, AppDispatch} from "./store";
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 // test2 commit
+// test2 第二次提交
